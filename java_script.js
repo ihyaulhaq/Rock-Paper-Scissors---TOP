365e7b975ca7320a4ffa6c923b92804207004c2b
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-03-22 14:29:56 +0700

    Adding function for play 5 round
    Adding function to repeat 1 game for 5 times
    and print in console the result

--- java_script.js
+++ java_script.js
@@ -14,21 +14,6 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let player_input = prompt("rock paper or scissors ?").toLowerCase();
-
-  if (
-    player_input === "rock" ||
-    player_input === "scissors" ||
-    player_input === "paper"
-  ) {
-    document.getElementById(
-      "player_input_txt"
-    ).innerHTML += `Your choice is <b>${player_input} !!</b>`;
-  } else {
-    document.getElementById(
-      "player_input_txt"
-    ).innerHTML += `Please input valid text`;
-  }
-
   return player_input;
 }
 
@@ -53,14 +38,36 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+  humanScore = 0;
+  computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection);
 
-console.log(humanScore);
-console.log(humanSelection);
-console.log(computerScore);
-console.log(computerSelection);
+    console.log("round " + (i + 1));
+    if (
+      humanSelection === "rock" ||
+      humanSelection === "scissors" ||
+      humanSelection === "paper"
+    ) {
+      console.log("your choice " + humanSelection);
+    } else {
+      console.log(`Please input valid text`);
+    }
+    console.log("comp choice " + computerSelection);
+    console.log("your score = " + humanScore);
 
-// document.getElementById("comp_c").innerHTML += `computer choice is <b>${getComputerChoice()} !!</b>`;
+    console.log("comp score = " + computerScore);
+  }
+
+  if (humanScore > computerScore) {
+    console.log("YOU WIN !!");
+  } else {
+    console.log("YOU LOSE !!");
+  }
+}
+
+// playGame();
