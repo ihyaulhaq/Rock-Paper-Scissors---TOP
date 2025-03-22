7f4cd1c9c589f52b5fc65b6d1cb5599eef9f5749
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-03-22 13:22:31 +0700

    Adding function for display user input
    Add function to store what user input and return it than display to html

--- java_script.js
+++ java_script.js
@@ -13,5 +13,24 @@ function getComputerChoice() {
   return choice;
 }
 
-document.getElementById("comp_c").innerHTML += ` <b>${getComputerChoice()} !!</b>`;
+function getHumanChoice() {
+  let player_input = prompt("rock paper or scissors ?").toLowerCase();
 
+  if (
+    player_input === "rock" ||
+    player_input === "scissors" ||
+    player_input === "paper"
+  ) {
+    document.getElementById(
+      "player_input_txt"
+    ).innerHTML += `Your choice is <b>${player_input} !!</b>`;
+  } else {
+    document.getElementById(
+      "player_input_txt"
+    ).innerHTML += `Please input valid text`;
+  }
+
+  return player_input;
+}
+
+// document.getElementById("comp_c").innerHTML += `computer choice is <b>${getComputerChoice()} !!</b>`;
